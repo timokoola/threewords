82d449877f8f4fdf9cc347655a0f7ff2513512b2
Timo Eemeli Koola <timo@timokoola.com>
2021-08-01 20:00:33 +0300

    some basic changes here

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,6 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   siteName: "11ty Netlify Jumpstart",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed, and social share preview images.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Language Learning Blog of Timo Koola",
+  authorName: "Timo Koola",
 };
